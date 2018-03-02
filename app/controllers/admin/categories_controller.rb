c52b777bf32e8b96b8ad03594e6351564b4c1a79
PreranaShrestha <prerana.sh@gmail.com>
2018-03-02 21:56:45 +0000

    fix bugs

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,7 +9,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories]
     else
       render :new
     end
