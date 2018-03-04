3708531b443cc4514766e14be3be82b8bc88eafd
PreranaShrestha <prerana.sh@gmail.com>
2018-03-04 05:10:02 +0000

    Fix routes no two unique id in urls

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,7 @@
 class ReviewsController < ApplicationController
   def create
-    @product_id = params.require(:product_id)
-    @review = Review.new(review_params)
-    @review.product_id = @product_id
+    product = Product.find(params[:product_id])
+    @review = product.reviews.new(review_params)
     @review.user_id = current_user.id
     if @review.save
       redirect_to :back
