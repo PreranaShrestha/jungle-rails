5dc4574c514dd8fcf89b95a8aa9b3594fc169fbd
PreranaShrestha <prerana.sh@gmail.com>
2018-03-06 15:34:35 +0000

    Add product-quantity-adjustment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   validates :password, length: { minimum: 5 }
   def self.authenticate_with_credentials(email, password)
-    em = email.squish.downcase
-    p em
-    user = User.find_by_email(em)
+    user = User.where('lower(email) = ?', email.squish.downcase).first
     if user && user.authenticate(password)
      user
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe User, type: :model do
       @user.password.should == @user.password_confirmation
     end
     it 'Should return false if user email is presence' do
+       @user1.email = "TeST@TEST.com"
       expect(@user1).not_to be_valid
     end
     it 'Should return false if first name is not presence' do
@@ -29,19 +30,20 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
     it 'Should return false if password is less than 5' do
+      @user.password = "mona"
       expect(@user).to_not be_valid
     end
   end
 
-  before :example do
-    @user = User.create(email:"TEST@TEST.com",first_name: "Mona", last_name:"Rai", password: "mona", password_confirmation:"mona")
-    @user1 = User.create(email:"test@test.COM",first_name: "Mona", last_name:"Rai", password: "mona", password_confirmation:"mona")
+  # before :example do
+  #   @user = User.create(email:"TEST@TEST.com",first_name: "Mona", last_name:"Rai", password: "mona", password_confirmation:"mona")
+  #   @user1 = User.create(email:"test@test.COM",first_name: "Mona", last_name:"Rai", password: "mona", password_confirmation:"mona")
 
-  end
+  # end
 
   describe '.authenticate_with_credentials' do
     it 'Should return false if email and password is not valid' do
-      authenticate = User.authenticate_with_credentials("st@test.test", "monarai")
+      authenticate = User.authenticate_with_credentials("te@test.test", "monarai")
       expect(authenticate).to eq(nil)
     end
     it 'Should return pass if there is space' do
@@ -49,10 +51,8 @@ RSpec.describe User, type: :model do
       expect(authenticate).to_not eq(nil)
     end
     it 'Should return pass if email is casesensitive' do
-      authenticate = User.authenticate_with_credentials(" TEst@test.test", "monarai")
+      authenticate = User.authenticate_with_credentials("TEst@test.cOm", "monarai")
       expect(authenticate).to_not eq(nil)
     end
-
-
   end
 end
