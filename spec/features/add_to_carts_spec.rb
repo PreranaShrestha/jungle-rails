63e9d9f1320552db4ae4df303b9a20d3c98661a9
PreranaShrestha <prerana.sh@gmail.com>
2018-03-07 03:23:02 +0000

    Add rspec add_to_carts_spec.rb file

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see a specific products" do
+    # ACT
+
+    visit '/'
+    first('footer.actions a').click
+    sleep(5)
+    save_screenshot "test.png"
+
+     page.should have_content('My Cart (1)')
+  end
+end
